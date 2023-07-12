7c0c78a539fcb6421600601b8a2c8001794c1c41
sahil <sahil>
2023-07-12 11:46:50 +0530

    envport

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = require('./app.js')
 const mongoose = require('mongoose')
-const port = 3000
+const PORT=process.env.PORT || 3000
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
@@ -15,4 +15,4 @@ db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
 
 // Start Server
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
